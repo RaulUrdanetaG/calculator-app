a8f2e327e4a070d9e06992ae0455332e253e2cb9
RaulUrdanetaG <raul.urdaneta2612@gmail.com>
2023-07-21 14:58:49 -0500

    Finished UI, and added basic math functions

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+function add(addNumber1, addNumber2) {
+    return addNumber1 + addNumber2;
+};
+
+function sub(subNumber1, subNumber2) {
+    return subNumber1 - subNumber2;
+}
+
+function multiply(multNumber1, multNumber2) {
+    return multNumber1 * multNumber2;
+};
+
+function division(divNumber1, divNumber2) {
+    return divNumber1 / divNumber2;
+};
